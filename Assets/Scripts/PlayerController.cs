d10b52001896ca2de9b01d6a9b81338822acd9e6
lolstorm <ricardo.s.campos@ist.utl.pt>
2018-10-13 06:55:31 +0100

    added backward movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	public Transform vrCamera;
-	public float toggleAngle = 45.0f;
+	public float forwardAngle = 30.0f;
+	public float backwardAngle = 330.0f;
 	public float speed = 0.5f;
 	public bool moveForward;
+	public bool moveBackward;
 	private CharacterController cc;
 	// Use this for initialization
 	void Start () {
@@ -15,13 +17,19 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		moveForward = (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f) ? true : false;
-		if (Input.GetKeyDown ("f"))
-			moveForward = !moveForward;
+		moveForward = (vrCamera.eulerAngles.x >= forwardAngle && vrCamera.eulerAngles.x < 90.0f) ? true : false;
+		moveBackward = (vrCamera.eulerAngles.x <= backwardAngle && vrCamera.eulerAngles.x > 180.0f) ? true : false;
+		Debug.Log (vrCamera.eulerAngles.x + moveBackward.ToString() + moveForward.ToString());
 		if (moveForward && cc.isGrounded) {
 			Vector3 forward = vrCamera.TransformDirection (Vector3.forward);
+			forward.y = 0;
 			cc.Move (speed * forward);
-		} else {
+		} else if (moveBackward && cc.isGrounded) {
+			Vector3 backward = vrCamera.TransformDirection (Vector3.back);
+			backward.y = 0;
+			cc.Move (speed * backward);
+		}
+		else {
             cc.Move(Physics.gravity);
         }
 	}
