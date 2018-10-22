4c891dff17d09df1f17b0c14c5efaa979fe7d1c3
Cláudia Filipe <claudiafilipe7@hotmail.com>
2018-10-22 20:01:56 +0100

    Switch flames to minotaur

---
+++ Assets/Scripts/VRMinotaurCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRMinotaurCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "minotaur_hr-wind")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
