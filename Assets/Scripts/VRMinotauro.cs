47e3d003784d13ae5818f4f4e9841785bbdb1b1c
Cláudia Filipe <claudiafilipe7@hotmail.com>
2018-10-23 12:56:17 +0100

    Add things to minotaur
    Added a ridig body, a script and a mesh. Collision still doesnt work

---
+++ Assets/Scripts/VRMinotauro.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRMinotauro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void onCollisionEnter (Collision col)
+    {
+        if (col.gameObject.tag == "spear"){
+            Debug.Log("Hello", gameObject);
+        }
+    }
+}
--- Assets/Scripts/VRMinotauroCollision.cs
+++ Assets/Scripts/VRMinotauroCollision.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class VRMinotaurCollision : MonoBehaviour
+public class VRMinotauroCollision : MonoBehaviour
 {
     void OnCollisionEnter(Collision col)
     {
+         Debug.Log("Collision detected");
         if (col.gameObject.tag == "minotauro")
         {
             Destroy(col.gameObject);
