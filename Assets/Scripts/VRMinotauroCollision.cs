17d3be8a83526d22f60357c612d83b0a7d63c821
lolstorm <ricardo.s.campos@ist.utl.pt>
2018-10-23 19:51:32 +0100

    added flames

--- Assets/Scripts/VRMinotauroCollision.cs
+++ Assets/Scripts/VRMinotauroCollision.cs
@@ -3,17 +3,21 @@ using System.Collections;
 
 public class VRMinotauroCollision : MonoBehaviour
 {
+	public Transform flames;
+
 	void FixedUpdate()
     {
-
+		//GameObject.Find ("Big Flames").SetActive(false);
 		Ray ray = Camera.main.ScreenPointToRay (transform.position);
 		RaycastHit hit;
 
 		if (Physics.Raycast (ray, out hit, 30.0f)) {
-			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.red); 
+			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward)*30, Color.red); 
 			//Debug.DrawLine (ray.origin, hit.point, Color.green);
 			//Debug.Log (hit.transform.tag);
 			if (hit.transform.tag.Equals ("minotauro")) {
+				//GameObject.Find ("Big Flames").SetActive(true);
+				flames.gameObject.SetActive(true);
 				Destroy (hit.transform.gameObject);
 			}
 		} 
