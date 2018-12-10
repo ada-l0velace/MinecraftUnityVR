1faf98c0b882fe40859690424e1c2cc624a1564d
lolstorm <ricardo.s.campos@ist.utl.pt>
2018-12-10 06:22:06 +0000

    Updating unity because this version is flawed

--- Assets/Minecraft/Scripts/ClickToMove.cs
+++ Assets/Minecraft/Scripts/ClickToMove.cs
@@ -10,6 +10,7 @@ public class ClickToMove : MonoBehaviour
 	NavMeshAgent m_Agent;
 	RaycastHit m_HitInfo = new RaycastHit();
 	Vector3 startPosition;
+	bool commingBack = true;
 	void Start() {
 		startPosition = transform.position;
 		m_Agent = GetComponent<NavMeshAgent>();
@@ -21,11 +22,22 @@ public class ClickToMove : MonoBehaviour
 			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))*/
 		if (World.Instance.meshSurface != null && Vector3.Distance (startPosition, player.position) <= 15f) {
+			commingBack = false;
+			mob.GetComponent<Animator> ().SetBool ("walking", true);
 			m_Agent.destination = player.position;
-		}
-		else if(World.Instance.meshSurface != null)
+		} else if (World.Instance.meshSurface != null) {
 			m_Agent.destination = startPosition;
+			commingBack = true;
+		}
 
+		if (m_Agent.remainingDistance <= m_Agent.stoppingDistance && !commingBack) {
+			mob.GetComponent<Animator> ().SetBool ("walking", false);
+			transform.LookAt (player.position + new Vector3 (0, -0.5f, 0));
+			mob.GetComponent<Animator> ().SetTrigger ("attacking");
+		} else if (m_Agent.remainingDistance <= m_Agent.stoppingDistance) {
+			mob.GetComponent<Animator> ().SetBool ("walking", false);
+		}
+		/*
 		if (Vector3.Distance (transform.position, player.position) <= 1.5f) {
 			transform.LookAt(player.position+ new Vector3(0,-0.5f,0));
 			mob.GetComponent<Animator>().SetBool ("walking", false);
@@ -36,6 +48,6 @@ public class ClickToMove : MonoBehaviour
 		}
 		else
 			mob.GetComponent<Animator>().SetBool ("walking", false);
-		/*}*/
+		}*/
 	}
 }
